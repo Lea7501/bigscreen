267ce9e4a6418513da5bf7074c9c17b8aad1d5fd
lea abittan <lea.abittan@gmail.com>
2020-09-20 22:46:09 +0200

    fichiers bootstrap

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE-edge">
         <meta name="viewport" content="width=device.width, initial-scale=1">
         <title>Bigscreen</title>
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
     </head>
 <body>
 
@@ -17,6 +17,6 @@
   </div>  
 </div>
 
- <script src="/js/bootstrap.min.js"></script>
+ <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 </body>
 </html>
