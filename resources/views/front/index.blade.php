805095b10ba87231ac78b829005bd559fbd27a23
lea abittan <lea.abittan@gmail.com>
2020-09-21 00:47:53 +0200

    modification css sur la page d'index

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,11 +1,14 @@
 @extends('layout.master')
 
 @section('content')
-<h3>Merci de repondre a toutes les questions et de valider le formulaire en bas de page</h3>
+<div class='pb-5 pt-3 text-center mx-auto'>
+  <h2>Bonjour !</h2>
+  <h4>Merci de repondre a toutes les questions et de valider le formulaire en bas de page</h4>
+</div>
         <form method="post" action="/validate_message" class="col-md-4 mx-auto">
             {{ csrf_field() }}
-                <div class="form-group">
-                   <h3>Question 1/20 </h3> 
+                <div class="form-group pt-2">
+                   <h4>Question 1/20 </h4> 
                    <label for="email">Entrez votre mail</label>
                    <input type="email" class="form-control" name="email" placeholder="pierre.giraud@edhec.com" value="{{ old('email') }}">
                     @if ($errors->has('email'))
@@ -13,35 +16,29 @@
                     @endif
                 </div>
 
-                <div class="form-group">
-                   <h3>Question 2/20 </h3>
+                <div class="form-group pt-4">
+                   <h4>Question 2/20 </h4>
                    <label for="age">Entrez votre age</label>
                    <input type="number" class="form-control" name="age" placeholder="42" min="0" max="99" value="{{ old('age') }}">
-                   @if ($errors->has('age'))
-                      <p class="invalid-feedback">{{ $errors->first('age') }}</p>
-                   @endif
                </div>
 
-              <div class="form-group">
-               <h3>Question 3/20 </h3>
+              <div class="form-group pt-4">
+               <h4>Question 3/20 </h4>
                <label for="genre">Entrez votre sexe</label>
-                  <div class="custom-control custom-radio custom-control-inline">
-                    <input type="radio" name="genre" value="1" class="custom-control-input" checked>
-                    <label>Homme</label>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="genre" value="0" checked>
+                      <label class="form-check-label">Homme</label>
                   </div>
-                  <div class="custom-control custom-radio custom-control-inline">
-                    <input type="radio" name="genre" value="2" class="custom-control-input">
-                    <label>Femme</label>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="genre" value="1">
+                      <label class="form-check-label">Femme</label>
                   </div>
-                  <div class="custom-control custom-radio custom-control-inline">
-                    <input type="radio" name="genre" value="3" class="custom-control-input">
-                    <label>Prefere ne pas repondre</label>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="genre" value="3">
+                      <label class="form-check-label">Prefere ne pas répondre</label>
                   </div>
-                  @if ($errors->has('genre'))
-                    <p class="invalid-feedback">{{ $errors->first('genre') }}</p>
-                  @endif
-               </div>
-               <button class="btn btn-primary" type="submit">Valider</button>
+                </div>
+               <button class="btn btn-primary mt-4" type="submit">Valider</button>
          </form>
 
 @endsection
